bcd6103d59879afe1f018e7964d6d8a185927446
Marlon Couto <coutoozair@gmail.com>
2023-11-08 16:00:52 -0300

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+// import $ from "jquery";
+
+import "../styles/index.scss";
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import handlebars from "vite-plugin-handlebars";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, "partials"),
+    }),
+  ],
+});
