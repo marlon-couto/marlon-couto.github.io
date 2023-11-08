e999cc3651e7e03e533a558cb666445eb259d413
Marlon Couto <coutoozair@gmail.com>
2023-11-08 19:51:33 -0300

    feat: update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,28 @@
-// import $ from "jquery";
+import $ from "jquery";
+import Typed from "typed.js";
 
 import "../styles/index.scss";
+
+$(window).on("scroll", function () {
+  if (window.scrollY > 50) {
+    $(".navbar").addClass("navbar-sticky");
+  } else {
+    $(".navbar").removeClass("navbar-sticky");
+  }
+});
+
+$("#navbarNavOverlay").on("click", function () {
+  $("#navbarLinks").toggleClass("overlay");
+});
+
+$(function () {
+  const typed = new Typed("#typed", {
+    strings: ["Bootstrap", "JavaScript", "Angular", "C#", "ASP.NET"],
+    typeSpeed: 75,
+    loop: "infinite",
+    backDelay: 1000,
+    backSpeed: 75,
+    showCursor: true,
+  });
+  typed.start();
+});
