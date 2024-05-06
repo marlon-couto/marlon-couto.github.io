91ae54819e7a59f84c0775a172534c503c6fc9f7
Marlon Couto <ozair.couto@trieduc.com.br>
2024-05-06 17:45:28 -0300

    Remove jQuery como dependência do projeto

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,19 +1,31 @@
-import $ from "jquery";
 import Typed from "typed.js";
 
+const navbar = document.querySelector(".navbar") as HTMLElement;
+const navLinksInactive = document.querySelectorAll(
+  "#navbarLinks .nav-link.inactive",
+) as NodeListOf<HTMLElement>;
+
 // Adiciona efeito de transição à barra de navegação quando a pessoa usuária rola a tela a partir do topo.
-$(window).on("scroll", () => {
+window.addEventListener("scroll", () => {
   if (window.scrollY > 50) {
-    $(".navbar").addClass("navbar-sticky");
+    navbar.classList.add("navbar-sticky");
   } else {
-    $(".navbar").removeClass("navbar-sticky");
-    $("#navbarLinks .nav-link.inactive").removeClass("inactive");
+    navbar.classList.remove("navbar-sticky");
+    navLinksInactive.forEach((link) => {
+      link.classList.remove("inactive");
+    });
   }
 });
 
+const sections = document.querySelectorAll(
+  "#header, #aboutMe, #skills, #contact",
+) as NodeListOf<HTMLElement>;
+
+const linkAtivo = document.querySelector("nav .nav-link.active") as HTMLElement;
+
 // Realça na barra de navegação qual a seção atual da página que a pessoa usuária está.
 // https://codepen.io/mishunov/pen/opeRdL?editors=0010
-$(() => {
+document.addEventListener("DOMContentLoaded", () => {
   // Configuração para o IntersectionObserver.
   const config = {
     rootMargin: "-50px 0px -55%", // Define as margens para detecção de interseção
@@ -23,17 +35,17 @@ $(() => {
   // 1. Itera sobre as entradas observadas.
   // 2. Verifica se a entrada está intersectando.
   // 3. Chama a função de tratamento de interseção.
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        intersectionHandler(entry);
+  const observer = new IntersectionObserver((entradas) => {
+    entradas.forEach((entrada) => {
+      if (entrada.isIntersecting) {
+        intersectionHandler(entrada);
       }
     });
   }, config);
 
   // Observa cada elemento com os IDs especificados.
-  $("#header, #aboutMe, #skills, #contact").each(function () {
-    observer.observe(this);
+  sections.forEach((section) => {
+    observer.observe(section);
   });
 
   // Função para lidar com a interseção de elementos.
@@ -42,23 +54,24 @@ $(() => {
   // 3. Seleciona o elemento de navegação que deve estar ativo com base no ID da seção.
   // 4. Remove a classe 'active' do elemento de navegação atualmente ativo, se existir.
   // 5. Adiciona a classe 'active' ao elemento de navegação que deve estar ativo, se existir.
-  const intersectionHandler = (entry: IntersectionObserverEntry) => {
-    const sectionId = entry.target.id;
-    const currentlyActive = $("nav .nav-link.active");
-    const shouldBeActive = $(`nav .nav-link[href="#${sectionId}"]`);
+  const intersectionHandler = (entrada: IntersectionObserverEntry) => {
+    const sectionId = entrada.target.id;
+    const deveriaEstarAtivo = document.querySelector(
+      `nav .nav-link[href="#${sectionId}"]`,
+    );
 
-    if (!currentlyActive.is(shouldBeActive)) {
-      currentlyActive.removeClass("active");
-      shouldBeActive.addClass("active");
+    if (linkAtivo && deveriaEstarAtivo && linkAtivo != deveriaEstarAtivo) {
+      linkAtivo.classList.remove("active");
+      deveriaEstarAtivo.classList.add("active");
     }
   };
 });
 
 // Retorna o tema de cores atual (light ou dark).
-const getTheme = () => {
-  const theme = localStorage.getItem("theme");
-  if (theme) {
-    return theme;
+const obterTema = () => {
+  const tema = localStorage.getItem("tema");
+  if (tema) {
+    return tema;
   }
 
   return window.matchMedia("(prefers-color-scheme: dark)").matches
@@ -66,77 +79,96 @@ const getTheme = () => {
     : "light";
 };
 
+const iconeDeSol = document.querySelector("i.fa-sun") as HTMLElement;
+const iconeDeLua = document.querySelector("i.fa-moon") as HTMLElement;
+
 // Muda o ícone para combinar com o tema atual.
-const showToggleThemeIcons = () => {
-  if (getTheme() === "dark") {
-    $("i.fa-sun").show();
-    $("i.fa-moon").hide();
+const mostrarIconesDeTema = () => {
+  if (obterTema() === "dark") {
+    iconeDeSol.style.display = "block";
+    iconeDeLua.style.display = "none";
   } else {
-    $("i.fa-sun").hide();
-    $("i.fa-moon").show();
+    iconeDeSol.style.display = "none";
+    iconeDeLua.style.display = "block";
   }
 };
+
+const themeToggle = document.querySelector("#themeToggle") as HTMLInputElement;
 
 // Marca/desmarca a caixa de seleção que indica o tema atual,
 // salva o tema no armazenamento local do navegador
 // e adiciona um efeito de transição ao ícone.
-$(() => {
-  if (getTheme() === "dark") {
-    $("#themeToggle").prop("checked", true);
-    localStorage.setItem("theme", "dark");
-    $("i.fa-moon").addClass("animated-icon");
-    showToggleThemeIcons();
+(() => {
+  if (obterTema() === "dark") {
+    themeToggle.checked = true;
+    localStorage.setItem("tema", "dark");
+    iconeDeLua.classList.add("animated-icon");
+    mostrarIconesDeTema();
   } else {
-    $("#themeToggle").prop("checked", false);
-    localStorage.setItem("theme", "light");
-    $("i.fa-sun").addClass("animated-icon");
-    showToggleThemeIcons();
+    themeToggle.checked = false;
+    localStorage.setItem("tema", "light");
+    iconeDeSol.classList.add("animated-icon");
+    mostrarIconesDeTema();
   }
-});
+})();
 
 // Muda o tema de cores do Bootstrap se o switch for clicado
 // e salva o novo tema no armazenamento local.
-$("#themeToggle").on("click", () => {
-  if (getTheme() === "dark") {
-    localStorage.setItem("theme", "light");
-    showToggleThemeIcons();
+themeToggle.addEventListener("click", () => {
+  if (obterTema() == "dark") {
+    localStorage.setItem("tema", "light");
+    mostrarIconesDeTema();
   } else {
-    localStorage.setItem("theme", "dark");
-    showToggleThemeIcons();
+    localStorage.setItem("tema", "dark");
+    mostrarIconesDeTema();
   }
 
-  $("html").attr("data-bs-theme", getTheme());
+  document.documentElement.setAttribute("data-bs-theme", obterTema());
 });
+
+const navbarNavOverlay = document.querySelector(
+  "#navbarNavOverlay",
+) as HTMLElement;
+const navbarLinks = document.querySelector("#navbarLinks") as HTMLElement;
 
 // Exibe o overlay ao clicar no menu hambúrguer.
-$("#navbarNavOverlay").on("click", () => {
-  $("#navbarLinks").toggleClass("navbar__overlay");
+navbarNavOverlay.addEventListener("click", () => {
+  navbarLinks.classList.toggle("navbar__overlay");
 });
+
+const navbarNavOverlayInput = document.querySelector(
+  "#navbarNavOverlay > input",
+) as HTMLInputElement;
 
 // Garante que o menu hambúrguer esteja fechado ao carregar a página.
-$(() => {
-  $("#navbarNavOverlay > input").prop("checked", false);
-});
+(() => {
+  if (navbarNavOverlayInput) {
+    navbarNavOverlayInput.checked = false;
+  }
+})();
+
+const navItems = document.querySelectorAll(
+  "#navbarLinks > .nav-item",
+) as NodeListOf<HTMLInputElement>;
 
 // Fecha o menu overlay ao clicar em algum link.
-$("#navbarLinks > .nav-item").on("click", () => {
-  $("#navbarNavOverlay > input").prop("checked", false);
-  $("#navbarLinks").removeClass("navbar__overlay");
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    navbarNavOverlayInput.checked = false;
+    navbarLinks.classList.remove("navbar__overlay");
+  });
 });
 
 // Exibe o texto animado com efeito de máquina de escrever.
-$(() => {
+(() => {
   const typed = new Typed("#typedText", {
     strings: [
+      ".NET",
       "Angular",
-      "ASP.NET",
-      "Bootstrap",
       "C#",
       "JavaScript",
-      "jQuery",
       "Node.js",
       "React",
-      "Sass",
       "TypeScript",
     ],
     typeSpeed: 100,
@@ -144,67 +176,86 @@ $(() => {
     backDelay: 1000,
     backSpeed: 75,
   });
+
   typed.start();
-});
+})();
 
 // Calcula a posição atual da tela no navegador da pessoa usuária.
-const getWindowPosition = () => {
-  const windowHeight = $(window).height() ?? 0;
-  const scrollPosition = $(window).scrollTop() ?? 0;
+const obterPosicaoDaJanela = () => {
+  const windowHeight =
+    window.innerHeight || document.documentElement.clientHeight;
+
+  const scrollPosition =
+    window.scrollY ||
+    document.documentElement.scrollTop ||
+    document.body.scrollTop ||
+    0;
+
   return windowHeight + scrollPosition;
 };
 
 // As funções abaixo adicionam um efeito de animação ao conteúdo
 // quando a pessoa usuária rola a tela até a seção referida.
 
+const aboutMe = document.querySelector("#aboutMe") as HTMLElement;
+
 // Sobre Mim
-$(window).on("scroll", () => {
-  const aboutMe = $("#aboutMe");
-  if (getWindowPosition() > aboutMe.offset()!.top) {
-    aboutMe.addClass("about-me--animated");
+window.addEventListener("scroll", () => {
+  const aboutMeTop = aboutMe.getBoundingClientRect().top + window.scrollY;
+  if (obterPosicaoDaJanela() > aboutMeTop) {
+    aboutMe.classList.add("about-me--animated");
   }
 });
+
+const skills = document.querySelector("#skills") as HTMLElement;
 
 // Skills
-$(window).on("scroll", () => {
-  const skills = $("#skills");
-  if (getWindowPosition() > skills.offset()!.top) {
-    skills.addClass("skills--animated");
+window.addEventListener("scroll", () => {
+  const skillsTop = skills.getBoundingClientRect().top + window.scrollY;
+  if (obterPosicaoDaJanela() > skillsTop) {
+    skills.classList.add("skills--animated");
   }
 });
+
+const contact = document.querySelector("#contact") as HTMLElement;
 
 // Contato
-$(window).on("scroll", () => {
-  const contact = $("#contact");
-  if (getWindowPosition() > contact.offset()!.top) {
-    contact.addClass("contact--animated");
+window.addEventListener("scroll", () => {
+  const contactTop = contact.getBoundingClientRect().top + window.scrollY;
+  if (obterPosicaoDaJanela() > contactTop) {
+    contact.classList.add("contact--animated");
   }
 });
+
+const formInputs = document.querySelectorAll("#inputName, #inputEmail, #inputMessage") as NodeListOf<HTMLInputElement>;
+const inputName = document.querySelector('#inputName') as HTMLInputElement;
+const inputEmail = document.querySelector('#inputEmail') as HTMLInputElement;
+const inputMessage = document.querySelector('#inputMessage') as HTMLInputElement;
+const contactBtn = document.querySelector('#contactBtn') as HTMLButtonElement;
 
 // Habilita o botão de envio apenas se o formulário for válido.
-$("#inputName, #inputEmail, #inputMessage").on("input", () => {
-  const inputName = ($("#inputName").val() ?? "").toString();
-  const nameRegex = new RegExp(/^[a-zA-Z]+$/);
-  const isValidName = nameRegex.test(inputName) && inputName.length >= 2;
+formInputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    const inputNameText = inputName.value.toString();
+    const nameRegex = new RegExp(/^[a-zA-Z]+$/);
+    const isValidName = nameRegex.test(inputNameText) && inputNameText.length >= 2;
+    const emailRegex = new RegExp(
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
+    );
 
-  const emailRegex = new RegExp(
-    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
-  );
-  const isValidEmail = emailRegex.test(
-    ($("#inputEmail").val() ?? "").toString(),
-  );
-
-  const isValidMessage =
-    ($("#inputMessage").val() ?? "").toString().length >= 10;
-
-  if (isValidEmail && isValidMessage && isValidName) {
-    $("#contactBtn").attr("disabled", "false");
-  } else {
-    $("#contactBtn").attr("disabled", "true");
-  }
+    const isValidEmail = emailRegex.test(inputEmail.value.toString());
+    const isValidMessage = inputMessage.value.toString().length >= 10;
+    if (isValidEmail && isValidMessage && isValidName) {
+      contactBtn.disabled = false;
+    } else {
+      contactBtn.disabled = true;
+    }
+  });
 });
 
+const contactForm = document.querySelector('#contactForm') as HTMLElement;
+
 // Mostra mensagens de validação no formulário.
-$("#inputMessage").on("change", () => {
-  $("#contactForm").addClass("was-validated");
+inputMessage.addEventListener("change", () => {
+  contactForm.classList.add("was-validated");
 });
