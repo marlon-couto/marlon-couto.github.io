fad5ad208b6054b2717655df53c98b7b4a7492d6
Marlon Couto <coutoozair@gmail.com>
2024-08-26 10:44:41 -0300

    Atualiza arquivos

---
+++ src/scripts/store.ts
@@ -0,0 +1,25 @@
+import { atom } from "nanostores";
+
+const siteTitle = atom("Marlon Couto | Desenvolvedor Web");
+const sections = atom([
+  { constant: "HEADER", id: "header", description: "Início" },
+  { constant: "ABOUTME", id: "about-me", description: "Sobre Mim" },
+  { constant: "EXPERIENCE", id: "experience", description: "Experiência" },
+  { constant: "SKILLS", id: "skills", description: "Skills" },
+  { constant: "CONTACT", id: "contact", description: "Contato" },
+]);
+const getId = (constant: string) =>
+  sections.get().find((x) => x.constant === constant)?.id;
+const getHref = (constant: string) =>
+  "#" + sections.get().find((x) => x.constant === constant)?.id;
+const curriculumLink = atom(
+  "https://docs.google.com/document/d/1DCS6AFAudTjnK_K3m4E8oaipls8IqB3kUgOomCyx0qI/edit?usp=sharing",
+);
+
+export default {
+  siteTitle,
+  sections,
+  getHref,
+  getId,
+  curriculumLink,
+};
--- src/ui/ExperienceTimeline.tsx
+++ src/ui/ExperienceTimeline.tsx
@@ -2,16 +2,15 @@ import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
+import { v4 as uuid } from "uuid";
 import "react-vertical-timeline-component/style.min.css";
+import { useEffect, useState } from "react";
 
 const experiences = [
   {
-    key: "trybe",
     src: "trybe.jpg",
     alt: "Trybe",
     link: "https://betrybe.com",
-    backgroundColor: "rgb(33, 150, 243)",
-    color: "#fff",
     date: "Julho de 2023 a Novembro de 2023",
     title: "Summer Student",
     subtitle: "Remoto",
@@ -19,12 +18,9 @@ const experiences = [
       "Realização de monitorias para resolução de dúvidas, abordando tecnologias de back-end.",
   },
   {
-    key: "freelancer",
     src: "freelancer.svg",
     alt: "Freelancing",
     link: "",
-    backgroundColor: "rgb(33, 150, 243)",
-    color: "#fff",
     date: "Agosto de 2023 a Janeiro de 2024",
     title: "Desenvolvedor Web Full-Stack",
     subtitle: "Remoto",
@@ -32,12 +28,9 @@ const experiences = [
       "Criação e manutenção de sites e APIs usando soluções personalizadas para cada cliente.",
   },
   {
-    key: "trieduc",
     src: "trieduc.png",
     alt: "TRIEduc",
     link: "https://trieduc.com.br",
-    backgroundColor: "rgb(33, 150, 243)",
-    color: "#fff",
     date: "Janeiro de 2024 a Agosto de 2024",
     title: "Desenvolvedor C# Júnior",
     subtitle: "Remoto",
@@ -45,25 +38,48 @@ const experiences = [
       "Desenvolvimento e manutenção dos sistemas e bancos de dados da empresa.",
   },
 ];
+const defaultTheme = {
+  color: "var(--bs-emphasis-color)",
+  backgroundColor: "var(--bs-secondary-bg)",
+};
 
 const ExperienceTimeline = () => {
+  const [lineColor, setLineColor] = useState("");
+  useEffect(() => {
+    const getThemeColor = () => {
+      const theme = document.documentElement.getAttribute("data-bs-theme");
+      if (theme === "dark") {
+        return "#dee2e6";
+      } else {
+        return "#212529";
+      }
+    };
+
+    setLineColor(getThemeColor());
+    const observer = new MutationObserver(() => setLineColor(getThemeColor()));
+    observer.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ["data-bs-theme"],
+    });
+    return () => observer.disconnect();
+  }, []);
   return (
-    <VerticalTimeline className="experience__timeline">
+    <VerticalTimeline className="experience__timeline" lineColor={lineColor}>
       {experiences.map((experience) => (
         <VerticalTimelineElement
-          key={experience.key}
+          key={uuid()}
           className="vertical-timeline-element--work"
           contentStyle={{
-            background: experience.backgroundColor,
-            color: experience.color,
+            background: defaultTheme.backgroundColor,
+            color: defaultTheme.color,
           }}
           contentArrowStyle={{
-            borderRight: `7px solid ${experience.backgroundColor}`,
+            borderRight: `7px solid ${defaultTheme.backgroundColor}`,
           }}
           date={experience.date}
           iconStyle={{
-            background: experience.backgroundColor,
-            color: experience.color,
+            background: defaultTheme.backgroundColor,
+            color: defaultTheme.color,
           }}
           icon={
             <div className="experience__icon">
