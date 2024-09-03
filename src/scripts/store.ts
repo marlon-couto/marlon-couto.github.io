b0bb0399d7ba349f371982c83d5dbf89ce5c090a
Marlon Couto <coutoozair@gmail.com>
2024-09-03 09:41:13 -0300

    feat: Adiciona seção "Experiências"

--- src/scripts/store.ts
+++ src/scripts/store.ts
@@ -8,15 +8,55 @@ const sections = atom([
   { constant: "SKILLS", id: "skills", description: "Skills" },
   { constant: "CONTACT", id: "contact", description: "Contato" },
 ]);
-const getId = (constant: string) =>
-  sections.get().find((x) => x.constant === constant)?.id;
 const curriculumLink = atom(
   "https://docs.google.com/document/d/1DCS6AFAudTjnK_K3m4E8oaipls8IqB3kUgOomCyx0qI/edit?usp=sharing",
 );
+const experiences = atom([
+  {
+    src: "trybe.jpg",
+    alt: "Trybe",
+    link: "https://betrybe.com",
+    date: "Julho de 2023 a Novembro de 2023",
+    title: "Summer Student",
+    subtitle: "Remoto",
+    description:
+    "Realização de monitorias para resolução de dúvidas, abordando tecnologias de back-end.",
+  },
+  {
+    src: "freelancer.svg",
+    alt: "Freelancing",
+    link: "",
+    date: "Agosto de 2023 a Janeiro de 2024",
+    title: "Desenvolvedor Web Full-Stack",
+    subtitle: "Remoto",
+    description:
+    "Criação e manutenção de sites e APIs usando soluções personalizadas para cada cliente.",
+  },
+  {
+    src: "trieduc.png",
+    alt: "TRIEduc",
+    link: "https://trieduc.com.br",
+    date: "Janeiro de 2024 a Agosto de 2024",
+    title: "Desenvolvedor C# Júnior",
+    subtitle: "Remoto",
+    description:
+    "Desenvolvimento e manutenção dos sistemas e bancos de dados da empresa.",
+  },
+]);
+const defaultTheme = atom({
+  color: "var(--bs-emphasis-color)",
+  backgroundColor: "var(--bs-secondary-bg)",
+});
 
-export default {
+// Métodos auxiliares
+const getSectionId = (constant: string) =>
+  sections.get().find((x) => x.constant === constant)?.id;
+
+export {
   siteTitle,
   sections,
-  getId,
   curriculumLink,
+  experiences,
+  defaultTheme,
+  getSectionId,
 };
