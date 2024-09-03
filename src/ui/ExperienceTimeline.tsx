6fcd19810ee8f707c431e13667196a00f18158a5
Marlon Couto <coutoozair@gmail.com>
2024-09-03 09:41:25 -0300

    feat: Adiciona seção "Experiências" e atualiza componente React

--- src/ui/ExperienceTimeline.tsx
+++ src/ui/ExperienceTimeline.tsx
@@ -3,48 +3,14 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import { v4 as uuid } from "uuid";
-import "react-vertical-timeline-component/style.min.css";
 import { useEffect, useState } from "react";
+import { experiences, defaultTheme } from "../scripts/store";
+import "react-vertical-timeline-component/style.min.css";
 
-const experiences = [
-  {
-    src: "trybe.jpg",
-    alt: "Trybe",
-    link: "https://betrybe.com",
-    date: "Julho de 2023 a Novembro de 2023",
-    title: "Summer Student",
-    subtitle: "Remoto",
-    description:
-      "Realização de monitorias para resolução de dúvidas, abordando tecnologias de back-end.",
-  },
-  {
-    src: "freelancer.svg",
-    alt: "Freelancing",
-    link: "",
-    date: "Agosto de 2023 a Janeiro de 2024",
-    title: "Desenvolvedor Web Full-Stack",
-    subtitle: "Remoto",
-    description:
-      "Criação e manutenção de sites e APIs usando soluções personalizadas para cada cliente.",
-  },
-  {
-    src: "trieduc.png",
-    alt: "TRIEduc",
-    link: "https://trieduc.com.br",
-    date: "Janeiro de 2024 a Agosto de 2024",
-    title: "Desenvolvedor C# Júnior",
-    subtitle: "Remoto",
-    description:
-      "Desenvolvimento e manutenção dos sistemas e bancos de dados da empresa.",
-  },
-];
-const defaultTheme = {
-  color: "var(--bs-emphasis-color)",
-  backgroundColor: "var(--bs-secondary-bg)",
-};
 
 const ExperienceTimeline = () => {
   const [lineColor, setLineColor] = useState("");
+
   useEffect(() => {
     const getThemeColor = () => {
       const theme = document.documentElement.getAttribute("data-bs-theme");
@@ -63,23 +29,24 @@ const ExperienceTimeline = () => {
     });
     return () => observer.disconnect();
   }, []);
+
   return (
     <VerticalTimeline className="experience__timeline" lineColor={lineColor}>
-      {experiences.map((experience) => (
+      {experiences.get().map((experience) => (
         <VerticalTimelineElement
           key={uuid()}
           className="vertical-timeline-element--work"
           contentStyle={{
-            backgroundColor: defaultTheme.backgroundColor,
-            color: defaultTheme.color,
+            backgroundColor: defaultTheme.get().backgroundColor,
+            color: defaultTheme.get().color,
           }}
           contentArrowStyle={{
-            borderRight: `7px solid ${defaultTheme.backgroundColor}`,
+            borderRight: `7px solid ${defaultTheme.get().backgroundColor}`,
           }}
           date={experience.date}
           iconStyle={{
-            backgroundColor: defaultTheme.backgroundColor,
-            color: defaultTheme.color,
+            backgroundColor: defaultTheme.get().backgroundColor,
+            color: defaultTheme.get().color,
           }}
           icon={
             <div className="experience__icon">
